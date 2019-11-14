8c5ae7c9036973bac95e3d1c5c56f038348edfb8
unknown <benjaminhocking25@gmail.com>
2019-11-14 21:18:16 +0000

    Removed int paramter entry

--- app/IP/ip_header.py
+++ app/IP/ip_header.py
@@ -1,1 +1,49 @@
-print("works")
+def createIPHead(_IPVersion, _tos, _identification, _flags, _ttl, _protocol, _sourceIP, _destinationIP, _totalLength):
+	#will use IPv4 and TCP protocol
+	if(len(_IPVersion)!=4):
+		return "ERROR 011: IP version must be 4 bit"
+	if(len(_tos)!=8):
+		return "ERROR 012: Type of service must be 8 bit"
+	if(len(_identification)!=16):
+		return "ERROR 013: Identification must be 16 bit"
+	if(len(_flags)!=3):
+		return "ERROR 014: flags must be 3 bit"
+	if(len(_ttl)!=8):
+		return "ERROR 015: time to live must be 8 bit"
+	if(len(_protocol)!=8):
+		return "ERROR 016: protocol must be 8 bit"
+	if(len(_sourceIP)!=32):
+		return "ERROR 017: souce IP must be 32 bit"
+	if(len(_destinationIP)!=32):
+		return "ERROR 018: destination IP must be 32 bit"
+	iPHead = []
+	iPHead.append(_IPVersion) #ip version (ipv4) - 4bit
+	iPHead.append(format(0, "04b")) #header length (initially set to 4) - 4bit
+	iPHead.append(_tos) #type of service - 8bit
+	iPHead.append(format(16, "016b")) #total length (initially set to 16) - 16bit
+	iPHead.append(_identification) #identification to identify each datagram sent by a host - 16bit
+	iPHead.append(_flags) #flags - 3bit
+	iPHead.append(format(0, "013b")) #fragment offset - 13bit
+	iPHead.append(_ttl) #time to live (max amount of routers through which the datagram can pass) - 8bit
+	iPHead.append(_protocol) #protocol used (TCP) - 8bit
+	iPHead.append(format(0, "016b")) #checksum - 16bit
+	iPHead.append(_sourceIP) #source IP - 32bit
+	iPHead.append(_destinationIP) # destination IP - 32bit
+	#still to: CALC HEADER LENGTH - [x] || CALC TOTAL LENGTH || CALC CHECKSUM
+	ipHeader = "".join(iPHead)
+	if(int(len(ipHeader)/32)!= len(ipHeader)/32):
+		return "ERROR 019: IP Header length is not a multiple of 32"
+	iPHead[1] = format(int(len(ipHeader)/32), "04b")
+	ipHeader = "".join(iPHead)
+	return ipHeader
+
+def createIP(_IPVersion, _tos, _identification, _flags, _ttl, _protocol, _sourceIP, _destinationIP, _data):
+	ipHeader = createIPHead(_IPVersion, _tos, _identification, _flags, _ttl, _protocol, _sourceIP, _destinationIP, format(0, "016b"))
+	ipPacket = ipHeader + _data
+	totalLen = format(int(len(ipPacket)/32), "016b")
+	ipHeader = createIPHead(_IPVersion, _tos, _identification, _flags, _ttl, _protocol, _sourceIP, _destinationIP, totalLen)
+	ipPacket = ipHeader + _data
+
+	return ipPacket
+
+print(createIP(format(4, "04b"), format(1, "08b"), format(1, "016b"), format(1, "03b"), format(1, "08b"), format(1, "08b"), format(1, "032b"), format(1, "032b"), format(100, "032b")))
--- app/TCP/tcp_header.py
+++ app/TCP/tcp_header.py
@@ -24,59 +24,49 @@ def createTCP(_sourcePort, _destinationPort, _sequenceNumber, _flagBitN, _window
 def fillTCPHeaderOptions( _options):
 	#mss = 32 bits long
 	optionsArr = []
-	if(_options>4294967296):
+
+	if(len(_options)!=32):
 		print("ERROR 002: Options MSS must be max 32 bits")
 		return format(0, "032b")
 
-	if(_options==0):
+	if(int(_options)==0):
 		_mss = 536
 		mss = format(_mss, "032b")
 		optionsArr.append(mss)
 		return mss
 	else:
 		_mss = _options
-		mss = format(_mss, "032b")
+		mss = format(int(_mss), "032b")
 		return mss
 
 def createTCPHead(_sourcePort, _destinationPort, _sequenceNumber, _flagBitN, _windowSize, _urgentPointer):
 	headerInitArr = [0]*10
 	_headerLength = 5
-	if(_sourcePort>65536):
+	if(len(_sourcePort)!=16):
 		return "ERROR 003: Source Port must be 16 bit"
-	if(_destinationPort>65536):
+	if(len(_destinationPort)!=16):
 		return "ERROR 004: Destination Port must be 16 bit"
-	if(_sequenceNumber>4294967296):
+	if(len(_sequenceNumber)!=32):
 		return "ERROR 005: Sequence number must be 32 bit"
-	if(_headerLength>16):
-		return "ERROR 006: Header Length must be 4 bit"
-	if(_flagBitN>64):
+	if(len(_flagBitN)!=6):
 		return "ERROR 007: Flag Bits must only be 6 bits"
-	if(_windowSize>65536):
+	if(len(_windowSize)!=16):
 		return "ERROR 008: Window Size must be 16 bits"
-	if(_urgentPointer>65536):
+	if(len(_urgentPointer)!=16):
 		return "ERROR 009: Urgent pointer must be 16 bits"
-
-
-	sourcePort = format(_sourcePort, "016b")
-	destinationPort = format(_destinationPort, "016b")
-	sequenceNumber = format(_sequenceNumber, "032b")
-	acknowledgementN = format(_sequenceNumber+1, "032b")
-	headerLength = format(5, "04b")
+	acknowledgementN = format(int(_sequenceNumber)+1, "032b")
 	reservedField = format(0, "06b")
-	flagBitN = format(_flagBitN, "06b")
-	windowSize = format(_windowSize, "016b")
 	checksum = format(0, "016b")
-	urgentPointer = format(_urgentPointer, "016b")
-	headerInitArr[0] = sourcePort
-	headerInitArr[1] = destinationPort
-	headerInitArr[2] = sequenceNumber
+	headerInitArr[0] = _sourcePort
+	headerInitArr[1] = _destinationPort
+	headerInitArr[2] = _sequenceNumber
 	headerInitArr[3] = acknowledgementN
-	headerInitArr[4] = headerLength
+	headerInitArr[4] = format(5, "04b")
 	headerInitArr[5] = reservedField
-	headerInitArr[6] = flagBitN
-	headerInitArr[7] = windowSize
+	headerInitArr[6] = _flagBitN
+	headerInitArr[7] = _windowSize
 	headerInitArr[8] = checksum
-	headerInitArr[9] = urgentPointer
+	headerInitArr[9] = _urgentPointer
 
 	_checksum = checksumTCPHeader(headerInitArr)
 
@@ -95,4 +85,4 @@ def checksumTCPHeader(tcpArray):
 
 dataEx = format(1203, "014b")
 
-print(createTCP(1,1,1,1,1,1,0,dataEx))
+print(createTCP(format(1, "016b"), format(1, "016b"), format(1, "032b"), format(1, "06b"), format(1, "016b"), format(1, "016b"), format(0, "032b"), format(1203, "014b")))
